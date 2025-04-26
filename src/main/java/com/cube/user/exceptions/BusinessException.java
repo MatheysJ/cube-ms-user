856eb45aac396b96f091cdd7da1a6851f659de5c
Matheus Júnior <matheusfpjunior@gmail.com>
2025-04-26 11:16:58 -0300

    refactor: solve warning by using lombok Getter annotation

--- src/main/java/com/cube/user/exceptions/BusinessException.java
+++ src/main/java/com/cube/user/exceptions/BusinessException.java
@@ -1,10 +1,12 @@
 package com.cube.user.exceptions;
 
 import com.cube.user.dtos.internal.ExceptionCode;
+import lombok.Getter;
 
+@Getter
 public class BusinessException extends RuntimeException {
 
-    private String code;
+    private final String code;
 
     public BusinessException (ExceptionCode exceptionCode) {
         super(exceptionCode.getMessage());
@@ -12,7 +14,4 @@ public class BusinessException extends RuntimeException {
         this.code = exceptionCode.name();
     }
 
-    public String getCode() {
-        return code;
-    }
 }
