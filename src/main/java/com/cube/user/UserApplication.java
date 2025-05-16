42ddf07a70d45b391e47c64f2819cc180af06729
Matheus Júnior <matheusfpjunior@gmail.com>
2025-05-15 22:23:43 -0300

    feat: add eureka

--- src/main/java/com/cube/user/UserApplication.java
+++ src/main/java/com/cube/user/UserApplication.java
@@ -2,9 +2,11 @@ package com.cube.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.cube.user.clients")
 public class UserApplication {
 
